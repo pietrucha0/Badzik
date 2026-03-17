1c9b45f1d5e0ada39df04b366591ddd44b68aec2
pietrucha0 <u146_natwoj_waw@technischools.com>
2026-03-17 22:40:28 +0100

    created prisma schema with base models

---
+++ src/app/page.tsx
@@ -0,0 +1,16 @@
+'use client'
+
+import { Search, Plus, User, CheckCircle2 } from "lucide-react";
+import {useState, useEffect} from 'react'
+
+function StatCard({label, value, color}: {label:string; value:string; color:string;}){
+
+}
+
+export default function Dashboard() {
+
+  return (
+    <div>Elo</div>
+
+  );
+}
